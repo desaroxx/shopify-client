56ac527c7428b52a6af4207f822d497bf69a0302
Ken Steiner <kenosteiner@gmail.com>
2025-03-08 14:09:10 +0100

    update vercel build path

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,9 @@ import {oxygen} from '@shopify/mini-oxygen/vite';
 import {vitePlugin as remix} from '@remix-run/dev';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
-declare module "@remix-run/server-runtime" {
+declare module '@remix-run/server-runtime' {
   interface Future {
-    v3_singleFetch: true;
+    v3_singleFetch: boolean;
   }
 }
 
